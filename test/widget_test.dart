d9983b15383a3a7f4e3bb5dc02a79e324ebfcff5
CulesBao <102230334@sv1.dut.udn.vn>
2025-10-22 20:41:33 +0700

    Add .gitkeep file to assets/images directory for tracking empty folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:find_job_mobile/app/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Verify that our counter starts at 0.
