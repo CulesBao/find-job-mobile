a84eb967ded9d81176075d044c804c7e9553498f
CulesBao <102230334@sv1.dut.udn.vn>
2025-10-25 09:18:59 +0700

    refactor: remove email validation regex from sign-in screen

--- lib/modules/auth/screens/sign_in_screen.dart
+++ lib/modules/auth/screens/sign_in_screen.dart
@@ -117,12 +117,6 @@ class _SignInScreenState extends State<SignInScreen> {
                     if (value == null || value.isEmpty) {
                       return 'Please enter your email';
                     }
-                    final emailRegex = RegExp(
-                      r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-                    );
-                    if (!emailRegex.hasMatch(value)) {
-                      return 'Please enter a valid email';
-                    }
                     return null;
                   },
                 ),
